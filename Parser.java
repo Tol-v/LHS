4162c60a64fe76353ca8ffe95651e0848b25c3bc
Tol-v <Tol-v>
2016-04-29 10:18:10 +0300

    read input files()
    Signed-off-by: Tol-v <Tol-v>

--- Parser.java
+++ Parser.java
@@ -25,4 +25,41 @@ public class Parser {
             System.out.println("I/O Exception while reading " + inputName);
         }
     }
+
+    private void readInputFile() throws IOException {
+        String unparsedQuery = readLines(QUERY_NUM_LINES, scanner).get(0); //query = one line
+        Predicate query = readPredicate(unparsedQuery);
+        ArrayList<Rule> KB_Rules = new ArrayList<Rule>();
+
+        int numClauses = Integer.parseInt(readLines(NUMSENTENCES_NUM_LINES, scanner).get(0));
+
+        ArrayList<String> unparsedPredicates = readLines(numClauses, scanner);
+
+        for(String unparsedPredicate : unparsedPredicates) {
+            Rule currRule = null;
+            if(!hasLHS(unparsedPredicate)) {
+                Predicate pred =  readPredicate(unparsedPredicate);
+                currRule = new Rule(pred, null);
+            }
+            else {//separate each predicate into RH and LH sides
+                String rhs = getRHS(unparsedPredicate);
+                String lhs = getLHS(unparsedPredicate);
+                ArrayList<String> lhsArr = separateLHS(lhs);
+
+                ArrayList<Predicate> lhsPreds = new ArrayList<Predicate>();
+                for(String lhsPred : lhsArr) {
+                    Predicate pred = readPredicate(lhsPred);
+                    lhsPreds.add(pred);
+                }
+
+                Predicate rhsPred =  readPredicate(rhs);
+
+                currRule = new Rule(rhsPred, lhsPreds);
+            }
+
+            KB_Rules.add(currRule);
+        }
+
+        this.KB = new KnowledgeBase(query, KB_Rules);
+    }
 }
