4b7f036bf8e84f7095cc08422c2613906599e2be
Tol-v <Tol-v>
2016-04-29 09:45:37 +0300

    kb skel
    Signed-off-by: Tol-v <Tol-v>

--- KnowledgeBase.java
+++ KnowledgeBase.java
@@ -1,5 +1,15 @@
 import java.util.ArrayList;
 
 public class KnowledgeBase {
+    private Predicate query;
+    private ArrayList<Rule> rules;
 
+    public KnowledgeBase(Predicate query, ArrayList<Rule> rules) {
+        this.query = query;
+        this.rules = rules;
+    }
+
+    public Predicate getQuery() {
+        return this.query;
+    }
 }
