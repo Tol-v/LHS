d219b90529ae4d0f0227a3be63e688b3e792d062
Tol-v <Tol-v>
2016-04-29 10:15:29 +0300

    Parser class skel
    Signed-off-by: Tol-v <Tol-v>

--- Parser.java
+++ Parser.java
@@ -3,5 +3,26 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Parser {
+    private final int QUERY_NUM_LINES = 1;
+    private final int NUMSENTENCES_NUM_LINES = 1;
 
+    private Scanner scanner;
+    private String outputName;
+    private KnowledgeBase KB;
+
+    public Parser(String inputName, String outputName) throws FileNotFoundException {
+
+        File inputFile = new File(inputName);
+        scanner = new Scanner(inputFile);
+
+        this.outputName = outputName;
+        //setupOutputFile();
+
+        try {
+            readInputFile();
+        }
+        catch(IOException ioe) {
+            System.out.println("I/O Exception while reading " + inputName);
+        }
+    }
 }
