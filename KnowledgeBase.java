35568256f944fa81b4c2c58fde80fa5103307faf
Tol-v <Tol-v>
2016-04-29 09:52:40 +0300

    kb fetch rules
    Signed-off-by: Tol-v <Tol-v>

--- KnowledgeBase.java
+++ KnowledgeBase.java
@@ -12,4 +12,20 @@ public class KnowledgeBase {
     public Predicate getQuery() {
         return this.query;
     }
+
+
+    public ArrayList<Rule> fetchRules(Predicate goal) {
+        ArrayList<Rule> rules = new ArrayList<Rule>();
+
+        for(Rule currRule : this.rules) {
+            String rhsName = currRule.getRHS().getName();
+
+            if(rhsName.equals(goal.getName())) {
+                if(nonVarArgsMatch(goal, currRule.getRHS()))
+                    rules.add(currRule);
+            }
+        }
+
+        return rules;
+    }
 }
